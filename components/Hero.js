a9bebf11eb320f97d6a1ab2e862393ad54cf091f
C-A-Y-D-E <notyetfamousprk@gmail.com>
2023-05-12 01:17:01 +0530

    k

--- components/Hero.js
+++ components/Hero.js
@@ -9,15 +9,16 @@ const Hero = () => {
             <img src="/text.png" className="max-w-[500px] w-full" />
             <div className="flex gap-4 items-center mt-6">
               <a
-                href=""
+                href="https://app.uniswap.org/#/swap?outputCurrency=0x5e86075111f1d3a11270c376e89934e3f43e873a"
                 target="_blank"
                 className="py-2 shadw border border-black m px-8 bg-[linear-gradient(180deg,#02F0F7_0%,#5BD4BD_100%)]"
               >
                 Buy Now
               </a>
               <a
-                href=""
+                href="https://www.dextools.io/app/en/ether/pair-explorer/0x1e73e8eb158af73a9f2871cce84e3cc748c71fa1"
                 target="_blank"
+                rel="noreferrer"
                 className="py-2 shadw border border-black m px-8 bg-[linear-gradient(180deg,#02F0F7_0%,#5BD4BD_100%)]"
               >
                 Chart
